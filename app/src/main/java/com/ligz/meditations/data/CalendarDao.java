ac48c8634c33d2f9cc343b67c0fe9a3e0420d5e1
esmusssein777 <380449395@qq.com>
2019-06-02 15:01:58 +0800

    修改按钮

--- app/src/main/java/com/ligz/meditations/data/CalendarDao.java
+++ app/src/main/java/com/ligz/meditations/data/CalendarDao.java
@@ -2,9 +2,13 @@ package com.ligz.meditations.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.ligz.meditations.model.Calendar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 关于日历评价的dao层
@@ -53,6 +57,55 @@ public class CalendarDao {
         db.close();
         cHelper.close();
         return row > 0 ? 1 : 0;
+    }
+
+    public Calendar getCalendarByTime(int year, int month, int day) {
+        List<Calendar> calendars = new ArrayList<>();
+        String[] values = {String.valueOf(year), String.valueOf(month), String.valueOf(day)};
+        SQLiteDatabase db = cHelper.getReadableDatabase();
+        Cursor cursor = db.query(CalendarDBConfig.CALENDAR_TABLE_NAME, null,
+                String.format("%s=? and %s=? and %s=?", CalendarDBConfig.CAL_YEAR,CalendarDBConfig.CAL_MONTH, CalendarDBConfig.CAL_DAY),
+                values, null, null, null);
+        Calendar calendar = new Calendar();
+        if (cursor.moveToFirst()) {
+            calendar.setId(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_ID)));
+            calendar.setTime(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_TIME)));
+            calendar.setYear(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_YEAR)));
+            calendar.setMonth(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_MONTH)));
+            calendar.setDay(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_DAY)));
+            calendar.setScoreForenoon(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_FORENOON)));
+            calendar.setScoreAfternoon(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_AFTERNOON)));
+            calendar.setScoreNight(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_NIGHT)));
+            calendar.setScoreDay(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_DAY)));
+        }
+        cursor.close();
+        db.close();
+        cHelper.close();
+        return calendar;
+    }
+
+    public List<Calendar> getAllCalendar() {
+        List<Calendar> calendars = new ArrayList<>();
+        SQLiteDatabase db = cHelper.getReadableDatabase();
+        Cursor cursor = db.query(CalendarDBConfig.CALENDAR_TABLE_NAME, null, null, null, null, null, null);
+        Calendar calendar;
+        while (cursor.moveToNext()) {
+            calendar = new Calendar();
+            calendar.setId(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_ID)));
+            calendar.setTime(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_TIME)));
+            calendar.setYear(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_YEAR)));
+            calendar.setMonth(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_MONTH)));
+            calendar.setDay(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_DAY)));
+            calendar.setScoreForenoon(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_FORENOON)));
+            calendar.setScoreAfternoon(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_AFTERNOON)));
+            calendar.setScoreNight(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_NIGHT)));
+            calendar.setScoreDay(cursor.getInt(cursor.getColumnIndex(CalendarDBConfig.CAL_SCORE_DAY)));
+            calendars.add(calendar);
+        }
+        cursor.close();
+        db.close();
+        cHelper.close();
+        return calendars;
     }
 
 
